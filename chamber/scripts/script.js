cc563faf895f0116b6180d64901b20641458b26a
Freeman Moyo <sirfmoyo@gmail.com>
2023-06-01 17:47:16 +0200

    done most of the stuff there

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -11,3 +11,18 @@ var formattedLastModified = lastModified.toLocaleDateString('en-US', options);
 // Update the year and last modified date in the footer
 document.getElementById("year").innerHTML = currentYear;
 document.getElementById("last-modified").innerHTML = formattedLastModified;
+document.getElementById("last-modified1").innerHTML = formattedLastModified;
+
+
+
+// not so sure about this 
+
+// Toggle menu items
+document.getElementById("navbar-toggle").addEventListener("click", function() {
+  var navbarMenu = document.getElementById("navbar-menu");
+  if (navbarMenu.style.display === "flex") {
+    navbarMenu.style.display = "none";
+  } else {
+    navbarMenu.style.display = "flex";
+  }
+});
